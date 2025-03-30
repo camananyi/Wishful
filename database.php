46d429573eeeaab968a78a6c45e1d4b84a2e248a
camananyi <62218927+camananyi@users.noreply.github.com>
2025-03-30 00:03:41 +0000

    test sign up

---
+++ database.php
@@ -0,0 +1,18 @@
+<?php
+
+$host = "sql3.freesqldatabase.com";
+$dbname = "sql3770314";
+$username = "sql3770314";
+$password = "yDQZ9XMRJ9";
+
+$mysqli = new mysqli(hostname: $host,
+                     username: $username,
+                     password: $password,
+                     database: $dbname);
+
+// check for connection error
+if ($mysqli->connect_errno) {
+    die("Connection error: " . $mysqli->connect_error);
+}
+
+return $mysqli;
