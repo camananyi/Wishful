83141f9b2a102dd688be1b25e51340f6de52a329
camananyi <62218927+camananyi@users.noreply.github.com>
2025-03-30 17:28:54 +0000

    Test 1 Login

---
+++ login.php
@@ -0,0 +1,20 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    
+    $mysqli = require __DIR__ . "/database.php";
+    
+    $sql = sprintf("SELECT * FROM user
+                    WHERE username = '%s'",
+                   $mysqli->real_escape_string($_POST["username"]));
+    
+    $result = $mysqli->query($sql);
+    
+    $user = $result->fetch_assoc();
+    
+    var_dump($user);
+    exit;
+}
+
+
+?>
