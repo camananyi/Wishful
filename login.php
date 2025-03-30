7fa21851fd3724e44ec598a81838cc65c96cf7ac
camananyi <62218927+camananyi@users.noreply.github.com>
2025-03-30 17:48:16 +0000

    Test 4 Login

--- login.php
+++ login.php
@@ -1,5 +1,7 @@
 <?php
 
+$is_invalid = false; 
+
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     
     $mysqli = require __DIR__ . "/database.php";
@@ -15,11 +17,39 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     if ($user) {
         
         if (password_verify($_POST["password"], $user["password"])) {
-            
+
             die ("Login Succesful");
         }
     }
+
+    $is_invalid = true;
 }
 
-
 ?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="login2.css" />
+  </head>
+  <body>
+    
+  <?php if ($is_invalid): ?>
+        <em>Invalid login</em>
+    <?php endif; ?>
+
+  <div class="square">
+    <form method="post" novalidate>
+    <p>Login!</p>
+    <label for="Uname">Username</label>
+    <input type="text" id="username" name="username">
+
+    <label for="Pass">Password</label>
+    <input type="text" id="password" name="password">
+
+    <a href="" class="button">Login</a>
+  </form>
+  </div>
+
+  </body>
+</html>
