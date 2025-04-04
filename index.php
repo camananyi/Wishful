8cb5b6274cfe8f46af2636f7050421bb8c339063
camananyi <62218927+camananyi@users.noreply.github.com>
2025-04-04 01:17:37 +0000

    new index.php

--- index.php
+++ index.php
@@ -2,6 +2,18 @@
 
 session_start();
 
+if (isset($_SESSION["user_id"])) {
+    
+    $mysqli = require __DIR__ . "/database.php";
+    
+    $sql = "SELECT * FROM user
+            WHERE id = {$_SESSION["user_id"]}";
+            
+    $result = $mysqli->query($sql);
+    
+    $user = $result->fetch_assoc();
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -32,6 +44,7 @@ session_start();
     <?php if (isset($user)): ?>
         
         <p>Hello <?= htmlspecialchars($user["name"]) ?></p>
+        <p>Hey <?= htmlspecialchars($user["username"]) ?></p>
         
     <?php else: ?>
         
