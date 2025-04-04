28f74db6c04f58cfd2a4e350c1e9d200990f0328
camananyi <62218927+camananyi@users.noreply.github.com>
2025-04-04 00:13:51 +0000

    Improved Login.php

--- login.php
+++ login.php
@@ -1,77 +1,59 @@
 <?php
+session_start(); // Start session at the beginning
 
-    if(array_key_exists('Login', $_POST)) {
-        login();
-    }
+$is_invalid = false; // Global variable
 
-$is_invalid = false; 
-
-function login() {
-    if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    
-        $mysqli = require __DIR__ . "/database.php";
-        
-        $sql = sprintf("SELECT * FROM user
-                        WHERE username = '%s'",
-                       $mysqli->real_escape_string($_POST["username"]));
-    
-        print_r($_POST["username"]);
-        print_r($sql);
-        
-        $result = $mysqli->query($sql);
-        
-        $user = $result->fetch_assoc();
-        
-        if ($user) {
-            print_r($_POST);
-            echo "Sign Up Succesful";
-            print_r($_POST["password"]);
-            print_r($user["password"]);
-    
-            
-            if (password_verify($_POST["password"], $user["password"])) {
-    
-                session_start();
-                echo "password verify called";
-                
-                $_SESSION["user_id"] = $user["id"];
-                
-                header("Location: index.php");
-                exit;
-            }
-        }
-    
-        $is_invalid = true;
-    } 
+if (isset($_POST['Login'])) {
+    login();
 }
 
+function login() {
+    global $is_invalid; // Ensure we modify the global variable
+    
+    if ($_SERVER["REQUEST_METHOD"] === "POST") {
+        $mysqli = require __DIR__ . "/database.php";
+        
+        $sql = sprintf("SELECT * FROM user WHERE username = '%s'",
+            $mysqli->real_escape_string($_POST["username"]));
+
+        $result = $mysqli->query($sql);
+        $user = $result->fetch_assoc();
+
+        if ($user && password_verify($_POST["password"], $user["password"])) {
+            $_SESSION["user_id"] = $user["id"];
+            header("Location: index.php");
+            exit;
+        }
+
+        $is_invalid = true;
+    }
+}
 ?>
 
 <!DOCTYPE html>
 <html>
-  <head>
+<head>
     <link rel="stylesheet" href="login2.css" />
-  </head>
-  <body>
+</head>
+<body>
     
-  <?php if ($is_invalid): ?>
-        <em>Invalid login</em>
-    <?php endif; ?>
+<?php if ($is_invalid): ?>
+    <em>Invalid login</em>
+<?php endif; ?>
 
-  <div class="square">
+<div class="square">
     <form method="post">
-    <p>Login!!</p>
-    <label for="Uname">Username</label>
-    <input type="text" id="username" name="username"
-        value="<?= htmlspecialchars($_POST["username"] ?? "") ?>">
+        <p>Login!!</p>
+        <label for="username">Username</label>
+        <input type="text" id="username" name="username"
+               value="<?= htmlspecialchars($_POST["username"] ?? "") ?>" required>
 
-    <label for="Pass">Password</label>
-    <input type="text" id="password" name="password">
+        <label for="password">Password</label>
+        <input type="text" id="password" name="password" required>
 
-    <input type="submit" name="Login"
-                class="button" value="Login" />
-  </form>
-  </div>
+        <input type="submit" name="Login" class="button" value="Login" />
+    </form>
+</div>
 
-  </body>
+</body>
 </html>
