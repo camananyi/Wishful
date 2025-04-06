2e9843372c423a312be124544f481ba7e823cb75
camananyi <62218927+camananyi@users.noreply.github.com>
2025-04-06 02:21:38 +0000

    new test.php

--- test.php
+++ test.php
@@ -1,4 +1,9 @@
 <?php
-    $mysqli = require __DIR__ . "/database.php";
-    echo "Database connected!";
+$mysqli = new mysqli("sql105.infinityfree.com", "if0_38682095", "97YQK2nv278B0", "if0_38682095_wishful");
+
+if ($mysqli->connect_error) {
+    die("Connection failed: " . $mysqli->connect_error);
+}
+
+echo "✅ Connection successful!";
 ?>
