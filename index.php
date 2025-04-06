85e2894366177c96be27cdeb9646bdf63e499947
camananyi <62218927+camananyi@users.noreply.github.com>
2025-04-06 04:49:09 +0000

    index.php new style for login users2

--- index.php
+++ index.php
@@ -24,18 +24,6 @@ if (isset($_SESSION["user_id"])) {
 </head>
 <body>
 
-<?php if (isset($user)): ?>
-    <div class="user-info">
-        <h2 class="title">Welcome back, <?= htmlspecialchars($user["name"]) ?>!</h2>
-        <p class="username-display">Username: <strong><?= htmlspecialchars($user["username"]) ?></strong></p>
-        <p><a class="logout-link" href="logout.php">Log out</a></p>
-    </div>
-<?php else: ?>
-    <div class="guest-info">
-        <p>Welcome! <a class="login-link" href="login.php">Log in</a> or <a class="signup-link" href="signup.html">sign up</a> to get started!</p>
-    </div>
-<?php endif; ?>
-
     <img src="https://assets.onecompiler.app/43954nvr8/439d86znu/_Bold%20Lettering%20Tumblr%20Banner.png" alt="Header Image" width="100%" height="165px">
     <section>
         <nav>
@@ -47,6 +35,19 @@ if (isset($_SESSION["user_id"])) {
                 <li style="float:right"><a href="#about">About</a></li>
             </ul>
         </nav>
+        
+        <?php if (isset($user)): ?>
+    <div class="user-info">
+        <h2 class="title">Welcome back, <?= htmlspecialchars($user["name"]) ?>!</h2>
+        <p class="username-display">Username: <strong><?= htmlspecialchars($user["username"]) ?></strong></p>
+        <p><a class="logout-link" href="logout.php">Log out</a></p>
+    </div>
+<?php else: ?>
+    <div class="guest-info">
+        <p>Welcome! <a class="login-link" href="login.php">Log in</a> or <a class="signup-link" href="signup.html">sign up</a> to get started!</p>
+    </div>
+<?php endif; ?>
+
         <article>
             <h1>London</h1>
             <p>London is the capital city of England. It is the most populous city in the United Kingdom, with a metropolitan area of over 13 million inhabitants.</p>
