7ea3b47d90cac010556a365164cf4242d37ca9eb
camananyi <62218927+camananyi@users.noreply.github.com>
2025-04-06 04:13:02 +0000

    New Database2

--- test.php
+++ test.php
@@ -7,7 +7,7 @@ error_reporting(E_ALL);
 $host = "db5017609052.hosting-data.io";
 $dbname = "dbs14095223";
 $username = "dbu2385668";
-$password = "Cam2011Code$MPk";
+$password = "Cam2011Code";
 
 // Try to connect
 $mysqli = new mysqli($host, $username, $password, $dbname);
