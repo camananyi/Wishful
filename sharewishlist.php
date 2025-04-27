35feb3e648c9bab545ac744d5e179c8a1e222b19
camananyi <62218927+camananyi@users.noreply.github.com>
2025-04-27 21:56:43 +0000

    share wishlist1

--- sharewishlist.php
+++ sharewishlist.php
@@ -2,6 +2,12 @@
 
 session_start();
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
 // create profile if does not exsist
 // write proflie id and whislist id in sharedwishlist database
 // send an email
