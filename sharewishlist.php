b03dce06944851c2e719d2a5c245d54af35f64ea
camananyi <62218927+camananyi@users.noreply.github.com>
2025-04-27 22:11:51 +0000

    share wishlist3

--- sharewishlist.php
+++ sharewishlist.php
@@ -8,20 +8,18 @@ session_start();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $friendUsername = $_POST['share_user'];
-    $WishlistId = isset($_GET['id']) ? intval($_GET['id']) : 0;
+    $WishlistId = intval($_POST['wishlist_id']);
     
     $host = "db5017609052.hosting-data.io";
     $dbname = "dbs14095223";
     $username = "dbu2385668";
     $password = "Cam2011Code";
 
-    $mysqli = new mysqli($host, $username, $password, $dbname);
-
     // Create connection
     $conn = new mysqli($host, $username, $password, $dbname);
 
     // check for connection error
-    if ($mysqli->connect_errno) {
+    if ($conn->connect_errno) {
         die("Connection error: " . $mysqli->connect_error);
     }
 
