caf01642f1d37bc15be9a50b4631a6d027a1d679
camananyi <62218927+camananyi@users.noreply.github.com>
2025-04-30 03:22:19 +0000

    mail share2

--- sharewishlist.php
+++ sharewishlist.php
@@ -10,7 +10,7 @@ session_start();
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require 'vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $friendUsername = $_POST['share_user'];
